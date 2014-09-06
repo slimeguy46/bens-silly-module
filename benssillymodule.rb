574c782695287d69fc7bfe934e84f7fec8e628d0
Benjamin Rausch <slimeguy46@gmail.com>
2014-09-06 14:03:39 -0700

    Started Work on a Geek Code generator method

--- benssillymodule.rb
+++ benssillymodule.rb
@@ -1,6 +1,6 @@
 module BensSillyModule
 	# Created by Benjamin Rausch!
-	$insults = ["How's that GED working out for you?","My cat has more brain cells than you.","The founding fathers would be so proud to know we let people like you vote.","I'm fairly confident Forrest Gump could destroy you in chess.","Is that your face or a Halloween mask?","If you were a religion, you'd be Scientology.","You make chimpanzees look sophisticated.","Fun fact: the average bum receives more hundred dollar bills in his tin cup each day than thoughts you'll ever have in your life."]
+	$insults = ["How's that GED working out for you?","My cat has more brain cells than you.","The founding fathers would be so proud to know we let people like you vote.","I'm fairly confident Forrest Gump could destroy you in chess.","Is that your face or a Halloween mask?","If you were a religion, you'd be Scientology.","You make chimpanzees look sophisticated.","Fun fact: the average bum receives more hundred dollar bills in his tin cup each day than thoughts you'll ever have in your life.","No matter how powerful we can make vacuums, there will never be one manufactured that sucks as much as you do."]
 	def BensSillyModule.meow(num)
 		num.times do
 			puts "meow"
@@ -59,7 +59,6 @@ module BensSillyModule
 	end
 	def BensSillyModule.to_cat(string) # Current version adds an extra space to the end. Working on removing that.
 		words = string.split(" ")
-		puts words
 		for thang in words do
 			lastchar = thang[-1]
 			if thang==words[-1]
@@ -199,7 +198,7 @@ module BensSillyModule
 		4
 	end
 	def BensSillyModule.generate_typical_username # Yes, this uses the base code from my horror movie generator, so sue me
-		terms = ["My","360","noscope","n0sc0pe","N05C0P3","420","blazeit","b1az3it","w33d","weed","sm0kin","720","180","quickscope","quikscope","quiksc0p3","l33t","1337","pr0","faze","faz3","0ptic","optic","sw@g","Y010","swag","5W@GGG","erryday","glazeit","OpTiC","FaZe","BlAzEiT","WeEd","NoSc0pE","SwAg","YoLo","sw@ggy","slayer","Slayer","Ki11er","Monster","DeStRoYeR","boi","crazy","CrAzY","dope","epic","1080","666","HASH","HaSh","hash","DeViL","123","321","haxor","hax","HAXX","PrO","da_b0mb",]
+		terms = ["My","360","Noscope","n0sc0pe","N05C0P3","420","Blazeit","B1az3it","w33d","Weed","Sm0kin","720","180","quickscope","Quikscope","Quiksc0p3","l33t","1337","pr0","Faz3","0ptic","Optic","Sw@g","Y010","Swag","5W@GGG","Erryday","Glazeit","OpTiC","FaZe","BlAzEiT","WeEd","NoSc0pE","SwAg","YoLo","sw@ggy","slayer","Slayer","Ki11er","Monster","DeStRoYeR","Boi","Crazy","Cray","Crayy","CrAzY","Dope","Epic","1080","666","HASH","HaSh","Hash","DeViL","123","321","Haxor","Hax","HAXX","PrO","Da_B0mb",]
 		prng = Random.new
 		term1 = terms[prng.rand(terms.length)]
 		term2 = terms[prng.rand(terms.length)]
@@ -211,6 +210,41 @@ module BensSillyModule
 		formula = formats[prng.rand(formats.length)]
 		formula
 	end
+	def BensSillyModule.geek_code # The Geek Code is a cool string of numbers that told info about a user's fandoms and computer skills. Though it was mainly used in the late '90s, it pops up every now and again on forums, so I'm building a CLU to do it justice. This is the one of the only times that the phrase 'C++' isn't in reference to a language. This CLU uses http://www.geekcode.com/geek.html as the source for all its info, and uses Geek Code version 3.12 
+		puts "Welcome to the idiot-proof Geek Code 3.12 generator created and maintained by Ben R! Answer the following questions about your geekiness, and you will be given a set of numbers and symbols at the end. That is your Geek Code."
+		puts "To begin, what is your vocation/study? (answer with number)"
+		puts "1. Business
+		2. Classics
+		3. Commercial Arts
+		4. Computer Management
+		5. Computer Science
+		6. Communications
+		7. Engineering
+		8. Education
+		9. Fine Arts
+		10. Government
+		11. Humanities
+		12. Information Technology
+		13. Jurisprudence (Law)
+		14. Library Science
+		15. Literature
+		16. Mass Communications
+		17. Math
+		18. Medicine
+		19. Music
+		20. Performing Arts
+		21. Philosophy
+		22. Science (Physics, Chemistry, Biology, etc.)
+		23. Social Science (Psychology, Sociology, etc.)
+		24. Technical Writing
+		25. Other
+		26. Undecided
+		26. No Qualifications
+		27. Jack of All Trades"
+		num = gets.chomp.to_i
+		# Find a way to verify they entered one of the correct integers
+	end
+	# To do: add method to translate geek code version 3.12 into legible format
 	# To do: access a dictionary in Ruby to make an indie band name generator
 	# To do: make a method comparing your complete genome to that of Kevin Bacon
 end
